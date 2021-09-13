47d4af6ad479ebb9704ee0ee0332c39cd76daa1c
Philipp Scheer <me@fipsi.at>
2021-09-13 10:04:15 +0200

    add static variable for threadpool

--- jarvis/ThreadPool.py
+++ jarvis/ThreadPool.py
@@ -7,33 +7,52 @@ from threading import Thread
 
 
 class ThreadPool:
-    """
-    ThreadPool stores a list of background threads and provides several features to control these threads
-    """
+    """ThreadPool stores a list of background threads and provides several features to control these threads"""
+
+    _all_threads = []
 
     def __init__(self, logging_instance: any = None) -> None:
-        """
-        Initialize an empty ThreadPool with a given `logging_instance`  
-        `logging_instance` should be a `Logger` instance
-        """
-        self.threads = []
+        """Initialize an empty ThreadPool with a given `logging_instance`  
+        `logging_instance` should be a `Logger` instance"""
+        self._threads = []
         self.logging_instance = logging_instance
 
     def register(self, target_function: any, thread_name: str, args: list = []) -> None:
-        """
-        Register a background thread and add it to the ThreadPool
+        """Register a background thread and add it to the ThreadPool
         * `target_function` specifies the function which should be run in background
         * `thread_name` specifies a short and descriptive name what this function is doing
-        * `args` specifies a list of arguments which should be passed to the function
-        """
+        * `args` specifies a list of arguments which should be passed to the function"""
         t = Thread(target=target_function, name=thread_name, args=args)
         t.start()
-        self.threads.append(t)
-
-    def stop_all(self, grace_period: int = 2) -> None:
-        """
-        Stop all threads in the ThreadPool  
-        Try terminating the threads before killing them
-        HAS NO EFFECT!
-        """
-        return True
+        t_object = {
+            "name": thread_name,
+            "function": target_function,
+            "thread": t
+        }
+        self._threads.append(t_object)
+        ThreadPool._all_threads.append(t_object)
+    
+    def status(self, internal_thread_object: dict = None, thread: Thread = None, thread_name: str = None, target_function: any = None) -> bool:
+        """Get the status of a background thread given a Thread object, thread name or target function.  
+        Returns a boolean whether it's alive or not and None if not found"""
+        if thread is not None:
+            return thread.is_alive()
+        elif internal_thread_object is not None:
+            for t in ThreadPool._all_threads:
+                if t["name"] == internal_thread_object.get("name", ""):
+                    return t["thread"].is_alive()
+        elif thread_name is not None:
+            for t in ThreadPool._all_threads:
+                if t["name"] == thread_name:
+                    return t["thread"].is_alive()
+        elif target_function is not None:
+            for t in ThreadPool._all_threads:
+                if t["function"] == target_function:
+                    return t["thread"].is_alive()
+        return None
+    
+    def all(self, include_children: bool = False):
+        if include_children:
+            return ThreadPool._all_threads
+        return self._threads
+        
